9ef4cce88c0a8c538a39bf1c248dc5585ff4e1a2
adamseelye <88736207+adamseelye@users.noreply.github.com>
2023-01-24 19:27:44 -0700

    fix typo

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -8,7 +8,7 @@ if (isset($_POST["submit"])) {
     require_once "functions.inc.php";
 
     if (emptyInputLogin($username, $pwd) !== false) {
-        header("location: ../signup.php?error=emptyinput");
+        header("location: ../login.php?error=emptyinput");
         exit();
     }
 
